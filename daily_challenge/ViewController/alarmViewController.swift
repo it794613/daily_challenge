304eef6ffe155ae526b6a58e4352b534f07b1742
it794613 <it794613@naver.com>
2022-10-04 18:08:50 +0900

    pickerView 까지 추가, 셀 정렬을 위한 버튼 누르면 피커뷰 나옴 까지 추가

--- daily_challenge/ViewController/alarmViewController.swift
+++ daily_challenge/ViewController/alarmViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 class alarmViewController : UIViewController{
     
     @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var pickerView: UIPickerView!
     
+    
+    
+    var pickerViewIsHidden : Bool = false
+    
+    let pickerData : [String] = ["전체", "핫딜", "프로필", "디테일"]
     
     let models : [Model] = [
         Model(type : "detail" ,text1: "hi", text2: "hellow", text3: "what's up"),
@@ -23,9 +29,22 @@ class alarmViewController : UIViewController{
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.isHidden = true
+        
 
     }
     
+    @IBAction func showPickerView(_ sender: Any) {
+        pickerViewIsHidden.toggle()
+        if pickerViewIsHidden == true {
+            pickerView.isHidden = false
+        }
+        else{
+            pickerView.isHidden = true
+        }
+    }
     
     
 }
@@ -51,3 +70,30 @@ extension alarmViewController : UITableViewDelegate{
 
 }
 
+//MARK: -PickerViewDelegate
+extension alarmViewController : UIPickerViewDelegate{
+    
+}
+
+//MARK: -PickerViewDataSource
+extension alarmViewController : UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // 선택되면, cell 에 데이터 타입에서 해당 픽과 동일한 정보만 정렬해서 리로드 하고
+        // endediting true 하기
+    }
+    
+    
+    
+}
